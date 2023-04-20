dcf0c1890536706f19668883f12dbe6fd61c34b4
BEOMS <jbs980612@gmail.com>
2023-04-20 11:02:32 +0900

    7주차

---
+++ src/week06/MyClass.java
@@ -0,0 +1,10 @@
+package week06;
+
+import java.awt.event.*;
+
+public class MyClass implements ActionListener{
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		System.out.println("beep");
+	}
+}
---
+++ src/week06/RemoteControl.java
@@ -0,0 +1,8 @@
+package week06;
+
+public interface RemoteControl {
+	public void turnOn(); //가전 제품을 켠다
+	public void turnOff();// 가전 제품을 끈다
+
+}
+
